66dd7a0c3ac7cf0ac8b943550d37f904605e82be
Aicha OUHAMOU <aichaouhamou2001@gmail.com>
2023-06-07 20:11:42 +0100

    style client

--- web/src/app/facture/facture.component.ts
+++ web/src/app/facture/facture.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class FactureComponent {
 
+  facturations = [
+    { 
+      montant: 500+' DH',  
+      nom: "Facture 001",
+      image:"../../assets/radeema.png",
+      description: "description ............"
+    },
+    { 
+      montant: 1000+' DH', 
+      nom: "Facture 002",
+      image:"../../assets/radeef.jpg",
+      description: "description ............"
+    },
+    {
+       montant: 1400+' DH',
+       nom: "Facture 003", 
+       image:"../../assets/radeef.jpg",
+       description: "description ............"
+    },
+    {
+      montant: 200+' DH',
+      nom: "Facture 004", 
+      image:"../../assets/radeema.png",
+      description: "description ............"
+   },
+       
+  ];
+
+    
+
 }
