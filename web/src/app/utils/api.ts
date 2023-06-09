576dbbdeb83f6269a3aab2ec7fa897b9faf12ebf
LAASRI Mohamed <84083555+GingFreecss2@users.noreply.github.com>
2023-06-09 14:55:38 +0100

    fixed donation route

--- web/src/app/utils/api.ts
+++ web/src/app/utils/api.ts
@@ -1,2 +1,2 @@
-export const Api = "http://localhost:8082"
-// export const Api = "https://server-production-73db.up.railway.app"
+//export const Api = "http://localhost:8082"
+export const Api = "https://server-production-73db.up.railway.app"
